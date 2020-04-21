475b55ad50407aaa3b3d58b723562269ebf18884
Patricia Matiesque <patymatiesque@gmail.com>
2020-04-21 17:54:30 -0300

    structure frontend finished

--- FutubeTube/FrontEnd/src/containers/Router/index.js
+++ FutubeTube/FrontEnd/src/containers/Router/index.js
@@ -8,11 +8,10 @@ import EditPasswordPage from "../EditPasswordPage/index"
 import CreateVideoPage from "../CreateVideoPage/index"
 import VideoDetailsPage from "../VideoDetailsPage/index"
 
-
 export const routes = {
   root: "/",
   signup: "/signup",
-  feed: "/feed",
+  login: "/login",
   edditPassword: "/newPassword",
   createVideo: "/video/create",
   videoDetails: "/video/details"
@@ -22,13 +21,12 @@ function Router(props) {
   return (
     <ConnectedRouter history={props.history}>
       <Switch>
-        <Route path={routes.root} component={LoginPage} exact />
+        <Route path={routes.root} component={FeedPage} exact />
         <Route path={routes.signup} component={SignupPage} exact />
-        <Route path={routes.feed} component={FeedPage} exact />
+        <Route path={routes.login} component={LoginPage} exact />
         <Route path={routes.edditPassword} component={EditPasswordPage} exact />
         <Route path={routes.createVideo} component={CreateVideoPage} exact />
-        <Route path={routes.createVideo} component={VideoDetailsPage} exact />
-
+        <Route path={routes.videoDetails} component={VideoDetailsPage} exact />
       </Switch>
     </ConnectedRouter>
   );
